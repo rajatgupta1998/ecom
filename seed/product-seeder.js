351150dc1606e73524f4fb4db4bb95f68677009b
Rajat Gupta <rajatgupta1998@acm.org>
2019-11-13 00:55:01 +0530

    Create dummy entries to seed the database

---
+++ seed/product-seeder.js
@@ -0,0 +1,24 @@
+var Product = require('../models/product');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopping');
+
+var products = [new Product({
+    imagePath: 'https://upload.wikimedia.org/wikipedia/en/7/7f/Cover_Art_of_Need_for_Speed_Heat.png',
+    title: 'NFS Heat',
+    price: 100
+}),
+new Product({
+    imagePath : 'https://upload.wikimedia.org/wikipedia/en/6/64/Need_for_Speed_Payback_standard_edition_cover_art.jpg',
+    title: 'NFS Payback',
+    price: 70
+}),
+new Product({
+    imagePath : 'https://upload.wikimedia.org/wikipedia/en/a/a9/Need_for_Speed_2015.jpg',
+    title: 'NFS 2015',
+    price: 50
+})
+];
+
+for( var i =0 ; i< products.length; i++){
+    products[i].save();
+}
