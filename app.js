422b2929fb085f89c8c08063813363d9cc2544ee
Rajat Gupta <rajatgupta1998@acm.org>
2019-11-13 09:35:52 +0530

    Use passport, passport-local, bcrypt and flash

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var expressHbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopping');
@@ -20,6 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'secret',resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
