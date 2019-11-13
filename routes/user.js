fbdcb73fe7ae8d9adda9d3f30997a39ad3a329e3
Rajat Gupta <rajatgupta1998@acm.org>
2019-11-13 12:18:21 +0530

    Router protection

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,16 @@ var passport =require('passport');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
+
+  
+router.get('/profile',isLoggedIn, function(req,res,next){
+  res.render('user/profile');
+});
+
+// router.use('/', notLoggedIn, function(req,res,next){
+//   next();
+// });
+
 router.get('/signup', function(req,res,next){
     var messages = req.flash('error');
     res.render('user/signup',{csrfToken: req.csrfToken(), messages:messages, hasErrors: messages.length > 0});
@@ -15,10 +25,6 @@ router.get('/signup', function(req,res,next){
     failureRedirect: '/user/signup',
     failureFlash: true
   }));
-  
-  router.get('/profile', function(req,res,next){
-    res.render('user/profile');
-  })
   
   router.get('/signin',function(req,res,next){
     var messages = req.flash('error');
@@ -31,4 +37,24 @@ router.get('/signup', function(req,res,next){
     failureFlash: true
   }));
   
+  router.get('/logout', function(req,res,next){
+    req.logout();
+    res.redirect('/');
+  })
   module.exports = router;
+
+  function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+      return next;
+    
+    }
+    res.redirect('/');
+  }
+
+  function notLoggedIn(req, res, next){
+    if(!req.isAuthenticated()){
+      return next;
+    
+    }
+    res.redirect('/');
+  }
