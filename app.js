04af90e5419354b86e97625637f08dea1d4e7182
Rajat Gupta <rajatgupta1998@acm.org>
2019-11-13 10:47:39 +0530

    Import express validator

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'secret',resave: false, saveUninitialized: false}));
 app.use(flash());
