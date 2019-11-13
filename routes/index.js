dc6e1c474bb3c10636abfac4e72c886d756bcdb5
Rajat Gupta <rajatgupta1998@acm.org>
2019-11-13 08:40:09 +0530

    Add csrf token and use it on signup

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,7 @@ router.get('/user/signup', function(req,res,next){
   res.render('user/signup',{csrfToken: req.csrfToken});
 });
 
+router.post('/user/signup', function(req,res,next){
+  res.redirect('/');
+})
 module.exports = router;
