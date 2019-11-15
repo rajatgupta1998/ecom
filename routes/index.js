ebb43885a20eac49c39bb0cba1269e5f1f0a578e
Rajat Gupta <rajatgupta1998@acm.org>
2019-11-15 23:03:11 +0530

    Add basic checkout page

--- routes/index.js
+++ routes/index.js
@@ -44,4 +44,12 @@ router.get('/shopping-cart',function(req, res, next){
   var cart = new Cart(req.session.cart);
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 })
+
+router.get('/checkout', function(req, res, next){
+  if(!req.session.cart){
+    return res.redirect('/shopping-cart');
+  }
+  res.render('shop/checkout',{total: Cart.totalPrice});
+});
+
 module.exports = router;
